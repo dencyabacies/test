599cef934711ec955865f3e6fd5eed7ee6c47916
dencygb <dency@abacies.com>
2017-04-27 18:11:04 +0530

    second commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    ////'dsn' => 'mysql:host=localhost;dbname=yii2basic',
 	'dsn' => 'sqlsrv:Server=al-teespring.database.windows.net;Database=teespring',
     'username' => 'abacies',
     'password' => 'Al@inno17!',

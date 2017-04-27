aeb38ce4251e34be57158a4cc8e1005a7da4b358
dencygb <dency@abacies.com>
2017-04-27 18:06:25 +0530

    initial commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+	'dsn' => 'sqlsrv:Server=al-teespring.database.windows.net;Database=teespring',
+    'username' => 'abacies',
+    'password' => 'Al@inno17!',
+    'charset' => 'utf8',
+];

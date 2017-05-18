8049667c69b959865a2728ae6699076728d3bda1
dencygb <dency@abacies.com>
2017-05-18 15:01:45 +0530

    DB reconfigured for demo

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     ////'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-	'dsn' => 'sqlsrv:Server=al-teespring.database.windows.net;Database=eqvision',
+	'dsn' => 'sqlsrv:Server=al-teespring.database.windows.net;Database=teespring',
     'username' => 'abacies',
     'password' => 'Al@inno17!',
     'charset' => 'utf8',

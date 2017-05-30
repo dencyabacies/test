d400e4a232e9b8d69dcdb4fa484c6ef166471268
dencygb <dency@abacies.com>
2017-05-30 16:49:18 +0530

    API Skeleton

--- modules/api/controllers/AuthController.php
+++ modules/api/controllers/AuthController.php
@@ -3,8 +3,19 @@
 namespace app\modules\api\controllers;
 
 use yii\rest\ActiveController;
+use yii\filters\auth\HttpBasicAuth;
 
 class AuthController extends ActiveController
 {
-    public $modelClass = 'app\models\User';
+
+    public $modelClass = 'app\models\APIUser';
+	
+	public function behaviors()
+	{
+		$behaviors = parent::behaviors();
+		$behaviors['authenticator'] = [
+			'class' => HttpBasicAuth::className(),
+		];
+		return $behaviors;
+	}
 }

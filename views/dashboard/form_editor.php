9fb34771e41e62ff53a73891f93ad91e3dddd5b8
dencygb <dency@abacies.com>
2017-05-30 12:25:42 +0530

    form disable

--- views/dashboard/form_editor.php
+++ views/dashboard/form_editor.php
@@ -38,7 +38,7 @@ $this->title = 'Form-Generator:'.$model->dashboard_name;
 			<div class="form-group cb col-md-2">
 			Publish
 			  <input type="hidden" name="tables[<?=$tab_name?>][is_published]" value='0'/>
-			  <input type="checkbox" data_tid="<?=$tab_name?>" id="<?=$tab_name?>_visible" class="un-select" name="tables[<?=$tab_name?>][is_published]" <?= $table['is_published']==1?'checked="true"':'' ?> />
+			  <input type="checkbox" data_tid="<?=$tab_name?>" id="<?=$tab_name?>_visible" class="un-select" name="tables[<?=$tab_name?>][is_published]" <?= $table['is_published']==1?'checked="true"':'' ?> value='1'/>
 			  <label for="<?=$tab_name?>_visible" class="check-box"></label>
 			</div>
 	<input type="hidden" name="tables[<?=$tab_name?>][model_name]" value="<?=$name?>" >

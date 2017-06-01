afe147987dc1a8884c4819ac12690ec8e17c7f54
Arivu-abacies <arivazhagan@abacies.com>
2017-06-01 17:21:23 +0530

    user manage for admin API request

--- modules/api/controllers/UserController.php
+++ modules/api/controllers/UserController.php
@@ -35,6 +35,12 @@ class UserController extends ActiveController
 		return ['hi'];
 	}
 
+	/*
+	 * Action for View Authorizated user Details
+	 * POST 
+	 * Return JSON
+	 */
+	 
 	public function actionViewUser(){		
 		$model = User::findOne(\Yii::$app->user->id);	 
 		if($model)
@@ -50,17 +56,29 @@ class UserController extends ActiveController
 		else { return ['Error' => $model->getErrors()];	}   
 	} 
 	
+	/*
+	 * Action for Authorizated user Can Deleted
+	 * POST 
+	 * Return JSON
+	 */
+	 
 	public function actionDeleteUser(){				
 		$model = User::findOne(\Yii::$app->user->id);							
 		  if($model->delete())
 		  {
 			$customer = Customer::find()->where(['eq_customer_id'=>\Yii::$app->user->id])->one();
 			if($customer->delete())
-			  return ['Success'];	
+			  return ['Success'=>"User Has Been Removed Successfully"];	
 		  }		  
 		 else { return ['Error' => $model->getErrors()]; }
 	}
 	
+	/*
+	 * Action for Authorizated user Can Update his Details
+	 * POST 
+	 * Return JSON
+	 */
+	 
 	public function actionUpdateUser(){				
 		$model = User::findOne(\Yii::$app->user->id);
 		$customer = Customer::find()->where(['eq_customer_id'=>\Yii::$app->user->id])->one();		
@@ -69,9 +87,83 @@ class UserController extends ActiveController
 			{
 			  $customer->updated_at = date("Y-m-d H:i:s");
 			  if($customer->save())
-				return ['Success'];			  
+				return ['Success'=>"User Details Has Been Updated Successfully"];			  
 			} 	
 			else { return ['Error' => $model->getErrors()]; }
 	}	
 
+	
+	/*
+	 * Action for View User Details By Admin User
+ 	 * POST 
+	 * Return JSON
+	 */
+	 
+	public function actionAdminViewUser($id){	
+		$model_check = User::findOne(\Yii::$app->user->id);
+		if($model_check->role == "admin")
+		{
+			$model = User::findOne($id);	 
+			if($model)
+			{
+				$datas = [
+				'username'=>$model->username,
+				'email'	 =>$model->email,
+				'role'	 =>$model->role,
+				];  
+			
+			   return $datas;
+			}  
+			else { return ['Error' => $model->getErrors()];	} 
+		}  
+		else { return ['Error' => "Access Denied"];	} 			
+	} 
+	
+	/*
+	 * Action for User Can Deleted By Admin User
+	 * POST 
+	 * Return JSON
+	 */
+	 
+	public function actionAdminDeleteUser($id){
+	  $model_check = User::findOne(\Yii::$app->user->id);
+	  if($model_check->role == "admin")
+	  {		
+		$model = User::findOne($id);							
+		  if($model->delete())
+		  {
+			$customer = Customer::find()->where(['eq_customer_id'=>$id])->one();
+			if($customer->delete())
+			  return ['Success' =>"User Has Been Removed Successfully"];	
+		  }		  
+		 else { return ['Error' => $model->getErrors()]; }
+	   }  
+		else { return ['Error' => "Access Denied"];	}  
+	}
+	
+	/*
+	 * Action for User Can Update his Details By Admin User
+	 * POST 
+	 * Return JSON
+	 */
+	 
+	public function actionAdminUpdateUser($id){	
+	  $model_check = User::findOne(\Yii::$app->user->id);
+	  if($model_check->role == "admin")
+	  {		
+		$model = User::findOne($id);
+		$customer = Customer::find()->where(['eq_customer_id'=>$id])->one();		
+		$model->email = $_POST['email'];
+			if($model->save())
+			{
+			  $customer->updated_at = date("Y-m-d H:i:s");
+			  if($customer->save())
+				return ['Success' => "User Details Has Been Updated Successfully"];			  
+			} 	
+			else { return ['Error' => $model->getErrors()]; }
+	  }  
+		else { return ['Error' => "Access Denied"];	} 		
+	}	
+
+	
 }
